f8df45f71d7f764be2b62239cbe800b7d12e30bd
Jim Hayes <jhayes@sdsc.edu>
2018-02-26 14:30:36 -0800

    Set rpm make vars needed for Rocks v7.

--- src/julia/version.mk
+++ src/julia/version.mk
@@ -5,7 +5,7 @@ COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-julia
 VERSION        = 0.6.0
-RELEASE        = 0
+RELEASE        = 1
 PKGROOT        = /opt/julia
 
 SRC_SUBDIR     = julia
@@ -18,4 +18,5 @@ SOURCE_DIR     = julia-$(VERSION)
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
-RPM.EXTRAS     = AutoReq:No
+RPM.EXTRAS     = AutoReq:No\nAutoProv:No
+RPM.PREFIX     = $(PKGROOT)
